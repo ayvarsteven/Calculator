f6d1119b655b7a949bce0dfb898e5b65061ca6dd
Steven Ayvar <ayvarsteven@gmail.com>
2022-10-16 19:14:08 -0500

    adds buttons

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+// ==================== buttons ====================
+
+const addBtn = document.querySelector('#add')
+const subtractBtn = document.querySelector('#subtract')
+const multiplyBtn = document.querySelector('#multiply')
+const divideBtn = document.querySelector('#divide')
+const equalsBtn = document.querySelector('#equals')
+const clearBtn = document.querySelector('#clear')
+
+const oneBtn = document.querySelector('#one')
+const twoBtn = document.querySelector('#two')
+const threeBtn = document.querySelector('#three')
+const fourBtn = document.querySelector('#four')
+const fiveBtn = document.querySelector('#five')
+const sixBtn = document.querySelector('#six')
+const sevenBtn = document.querySelector('#seven')
+const eightBtn = document.querySelector('#eight')
+const nineBtn = document.querySelector('#nine')
+
 // ==================== basic functions ====================
 
 function sum(a, b) {
